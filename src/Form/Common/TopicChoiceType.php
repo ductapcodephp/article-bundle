fcd6afc003d36c7875feb3e2f1c2e5ee2ac18555
ductapcodephp <minhduc9a10dz@gmail.com>
2026-05-11 16:09:37 +0700

    [update]: select2 topic

--- src/Form/Common/TopicChoiceType.php
+++ src/Form/Common/TopicChoiceType.php
@@ -19,34 +19,28 @@ class TopicChoiceType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+
             'class' => Topic::class,
+
             'choice_label' => 'name',
+
             'multiple' => true,
+
+            'required' => false,
+
+            'expanded' => false,
+
             'query_builder' => function (EntityRepository $er) {
+
                 return $er->createQueryBuilder('t')
                     ->andWhere('t.deletedAt IS NULL')
                     ->orderBy('t.name', 'ASC');
             },
 
             'attr' => [
-                'data-controller' => 'select2',
-                'data-select2-placeholder-value' => '-- Chọn chủ đề --',
-                'class' => 'form-select form-select-sm',
+                'class' => 'form-select js-select2-topics',
             ],
-            'placeholder' => 'Chọn chủ đề',
         ]);
 
-        $resolver->setDefined(['data-select2-dropdown-parent-value']);
-        $resolver->setDefined(['data-select2-hidden-search-value']);
-
-        $resolver->setNormalizer('attr', function (Options $options, $value) {
-            if (isset($options['data-select2-dropdown-parent-value'])) {
-                $value['data-select2-dropdown-parent-value'] = $options['data-select2-dropdown-parent-value'];
-            }
-            if (isset($options['data-select2-hidden-search-value'])) {
-                $value['data-select2-hidden-search-value'] = $options['data-select2-hidden-search-value'];
-            }
-            return $value;
-        });
     }
 }
